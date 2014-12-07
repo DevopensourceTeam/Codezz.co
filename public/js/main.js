d8c11c7f74d821c68d1c4b8ed4020a248fcd576b
Oliver Montes <oliver.montes@devopensource.com>
2014-12-07 12:48:08 +0100

    add socket lib

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
 
   // Place JavaScript code here...
 
+    var socket = io.connect(window.location.href);
+	socket.on('greet', function (data) {
+		console.log(data);
+		socket.emit('respond', { message: 'Hello to you too, Mr.Server!' });
+	});
+
    $('.carousel').carousel({
         interval: 5000 //changes the speed
     });
