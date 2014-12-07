ecf1073ce3e071397e33bfd1f7966e1bee5d8d61
Marc Bernabeu Moreno <marc.bernabeu@devopensource.com>
2014-12-07 19:08:05 +0100

    add key mailgun

--- config/secrets.js
+++ config/secrets.js
@@ -24,8 +24,8 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
   mailgun: {
-    user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
-    password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
+    user: process.env.MAILGUN_USER || 'postmaster@sandboxf8cce430f0fc47838b5a44704cdbf15e.mailgun.org',
+    password: process.env.MAILGUN_PASSWORD || '123456'
   },
   
   mandrill: {
